fd43d30bd3cb84e1d989a0e9f7fa20517c34947d
djangonauta <igbrch@gmail.com>
2022-05-15 10:13:41 -0300

    fix: controladores

--- projetos/views.py
+++ projetos/views.py
@@ -5,31 +5,26 @@ from . import forms, models
 
 
 def criar(request):
-    if request.method == 'GET':
-        projeto_form = forms.ProjetoForm()
-        locais_inline_form = forms.LocalFormSet()
-        apelidos_inline_form = forms.ApelidoFormSet()
-        return shortcuts.render(request, 'projetos.html', {
-            'projeto_form': projeto_form,
-            'locais_inline_form': locais_inline_form,
-            'apelidos_inline_form': apelidos_inline_form,
-            'projetos': models.Projeto.objects.all(),
-        })
+    projeto_form = forms.ProjetoForm()
+    locais_inline_form = forms.LocalFormSet()
+    apelidos_inline_form = forms.ApelidoFormSet()
 
-    projeto_form = forms.ProjetoForm(request.POST, request.FILES)
-    locais_inline_form = forms.LocalFormSet(request.POST, request.FILES)
-    apelidos_inline_form = forms.ApelidoFormSet(request.POST, request.FILES)
-    if projeto_form.is_valid() and locais_inline_form.is_valid() and apelidos_inline_form.is_valid():
-        projeto = projeto_form.save()
+    if request.method == 'POST':
+        projeto_form = forms.ProjetoForm(request.POST, request.FILES)
+        locais_inline_form = forms.LocalFormSet(request.POST, request.FILES)
+        apelidos_inline_form = forms.ApelidoFormSet(request.POST, request.FILES)
 
-        locais_inline_form.instance = projeto
-        locais_inline_form.save()
+        if projeto_form.is_valid() and locais_inline_form.is_valid() and apelidos_inline_form.is_valid():
+            projeto = projeto_form.save()
 
-        apelidos_inline_form.instance = projeto
-        apelidos_inline_form.save()
+            locais_inline_form.instance = projeto
+            locais_inline_form.save()
 
-        messages.success(request, 'Projeto criado com sucesso')
-        return shortcuts.redirect('/')
+            apelidos_inline_form.instance = projeto
+            apelidos_inline_form.save()
+
+            messages.success(request, 'Projeto criado com sucesso')
+            return shortcuts.redirect('/')
 
     return shortcuts.render(request, 'projetos.html', {
         'projeto_form': projeto_form,
@@ -41,27 +36,22 @@ def criar(request):
 
 def editar(request, pk=None):
     projeto = shortcuts.get_object_or_404(models.Projeto, pk=pk)
-    if request.method == 'GET':
-        projeto_form = forms.ProjetoForm(instance=projeto)
-        locais_inline_form = forms.LocalFormSet(instance=projeto)
-        apelidos_inline_form = forms.ApelidoFormSet(instance=projeto)
-        return shortcuts.render(request, 'projetos.html', {
-            'projeto_form': projeto_form,
-            'locais_inline_form': locais_inline_form,
-            'apelidos_inline_form': apelidos_inline_form,
-            'projetos': models.Projeto.objects.all(),
-        })
+    projeto_form = forms.ProjetoForm(instance=projeto)
+    locais_inline_form = forms.LocalFormSet(instance=projeto)
+    apelidos_inline_form = forms.ApelidoFormSet(instance=projeto)
 
-    projeto_form = forms.ProjetoForm(request.POST, request.FILES, instance=projeto)
-    locais_inline_form = forms.LocalFormSet(request.POST, request.FILES, instance=projeto)
-    apelidos_inline_form = forms.ApelidoFormSet(request.POST, request.FILES, instance=projeto)
-    if projeto_form.is_valid() and locais_inline_form.is_valid() and apelidos_inline_form.is_valid():
-        projeto_form.save()
-        locais_inline_form.save()
-        apelidos_inline_form.save()
+    if request.method == 'POST':
+        projeto_form = forms.ProjetoForm(request.POST, request.FILES, instance=projeto)
+        locais_inline_form = forms.LocalFormSet(request.POST, request.FILES, instance=projeto)
+        apelidos_inline_form = forms.ApelidoFormSet(request.POST, request.FILES, instance=projeto)
 
-        messages.info(request, 'Projeto Atualizado com sucesso')
-        return shortcuts.redirect('/')
+        if projeto_form.is_valid() and locais_inline_form.is_valid() and apelidos_inline_form.is_valid():
+            projeto_form.save()
+            locais_inline_form.save()
+            apelidos_inline_form.save()
+
+            messages.info(request, 'Projeto Atualizado com sucesso')
+            return shortcuts.redirect('/')
 
     return shortcuts.render(request, 'projetos.html', {
         'projeto_form': projeto_form,
